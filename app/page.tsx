b1ad0203c88e6722da5dfa73346332f7be31f3b2
clockiz <clockiz@example.com>
2023-07-19 21:05:10 +0200

    creating dynamic routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
-import Image from 'next/image'
+"use client";
 
+
+import { useState, FormEvent} from "react";
 export default function Home() {
+  const [inputVal, setInputVal] = useState("");
+
+  const handleSubmit = (event:FormEvent) => {
+    event.preventDefault();
+    
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">      
-    </main>
+   <div>
+      <div>
+        <h1>Enter Your Name</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" 
+        placeholder='Type your name...' 
+        value = {inputVal}
+        onChange={(e) => setInputVal(e.target.value)} />
+        <button type='submit'> Predict Data </button>
+      </form>
+   </div>
   );
 }
