c0124b2f9725bbf6676e2f05da43bccee1b9da68
clockiz <clockiz@example.com>
2023-07-19 21:19:22 +0200

    added async funtions tofetch the prediction data

--- app/prediction/[name]/page.tsx
+++ app/prediction/[name]/page.tsx
@@ -1,3 +1,18 @@
+const getPredictedAge = async (name: string) => {
+    const res = await fetch(`https://api.agify.io/?name=${name}`);
+    return res.json();
+}
+
+const getPredictedGender = async (name: string) => {
+    const res = await fetch(`https://api.genderize.io/?name=${name}`);
+    return res.json();
+}
+
+const getPredictedCountry = async (name: string) => {
+    const res = await fetch(`https://api.nationalize.io/?name=${name}`);
+    return res.json();
+}
+
 interface Params {
     params: {name: string};
 }
