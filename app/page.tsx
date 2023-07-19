7ac4ab1c624cd5e4c32702f8dc71503d453973f3
clockiz <clockiz@example.com>
2023-07-19 21:38:30 +0200

    finished up with the stylings

--- app/page.tsx
+++ app/page.tsx
@@ -14,16 +14,18 @@ export default function Home() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className = "p-4 shadow-md bg-white rounded-md">
-        <h1>Enter Your Name</h1>
+      <div className="p-4 shadow-md bg-white rounded-md">
+        <h1 className="text-2xl font-semibold mb-4 text-black">Enter Your Name</h1>
       </div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="space-y-3">
         <input type="text"
           placeholder='Type your name...'
           value={inputVal}
           className="text-black"
           onChange={(e) => setInputVal(e.target.value)} />
-        <button type='submit'> Predict Data </button>
+        <button type='submit' className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+          Predict Data
+        </button>
       </form>
     </div>
   );
