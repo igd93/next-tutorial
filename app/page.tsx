ae9d7142c86781976e189d41c8de5a1170393a83
clockiz <clockiz@example.com>
2023-07-19 21:15:17 +0200

    now entering an input data redirects to a page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,30 @@
 "use client";
 
 
-import { useState, FormEvent} from "react";
+import { useState, FormEvent } from "react";
+import { useRouter } from "next/navigation";
 export default function Home() {
   const [inputVal, setInputVal] = useState("");
-
-  const handleSubmit = (event:FormEvent) => {
+  const { push } = useRouter();
+  const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    
+    push(`/prediction/${inputVal}`)
+
   };
 
   return (
-   <div>
+    <div>
       <div>
         <h1>Enter Your Name</h1>
       </div>
       <form onSubmit={handleSubmit}>
-        <input type="text" 
-        placeholder='Type your name...' 
-        value = {inputVal}
-        onChange={(e) => setInputVal(e.target.value)} />
+        <input type="text"
+          placeholder='Type your name...'
+          value={inputVal}
+          className="text-black"
+          onChange={(e) => setInputVal(e.target.value)} />
         <button type='submit'> Predict Data </button>
       </form>
-   </div>
+    </div>
   );
 }
